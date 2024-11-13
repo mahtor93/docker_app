47e13234e418885ed8652baef3a4785ea240484b
mahtor93 <mh.torneria@gmail.com>
2024-11-13 18:20:44 -0300

    creacion error al repetir password

--- auth_ev3/register/forms.py
+++ auth_ev3/register/forms.py
@@ -5,3 +5,12 @@ class RegistroForm(forms.Form):
     correo = forms.EmailField(label='Correo')
     psw = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
     psw2 = forms.CharField(label='Repetir Contraseña', widget=forms.PasswordInput)
+
+    def clean_psw2(self):
+        psw = self.cleaned_data.get('psw')
+        psw2 = self.cleaned_data.get('psw2')
+        
+        if psw and psw2 and psw != psw2:
+            self.add_error('psw2','Las contraseñas no coinciden.')
+            
+        return psw2
